e50807ec91b3811d94ec463ff3f31bb943b44288
rkdwn <kj2693119@gmail.com>
2023-04-06 16:43:32 +0900

    [Dev] api test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
-import Head from "next/head";
+import { useState } from "react";
+
 export default function Home() {
+  const [result, setResult] = useState<string>("");
+
+  const handleFetchApi = () => {
+    setResult("loading...");
+    fetch("/api/test")
+      .then(res => {
+        res.json().then(data => {
+          setResult(data.result);
+        });
+      })
+      .catch(e => {
+        console.log("# error : ", e.message);
+        setResult(JSON.stringify(e));
+      });
+  };
   return (
     <>
       <div>hello next.js</div>
+      <button
+        onClick={handleFetchApi}
+        style={{
+          width: 100,
+          height: 32
+        }}
+      >
+        Test
+      </button>
+      <div>{`result : ${result}`}</div>
     </>
   );
 }
